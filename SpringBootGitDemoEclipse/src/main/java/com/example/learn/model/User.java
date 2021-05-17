2f2beed794f30f53e11404290d0d368ab0b6da6b
Shubham kala <shubhamkala88@gmail.com>
2021-05-17 08:04:53 +0530

    user bean added in feature1

---
+++ SpringBootGitDemoEclipse/src/main/java/com/example/learn/model/User.java
@@ -0,0 +1,14 @@
+package com.example.learn.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	private String userName;
+	private int age;
+
+}
