ae83fe25f7df228763f57dab33c66344b068230e
Shubham kala <shubhamkala88@gmail.com>
2021-05-17 07:09:59 +0530

    first commit

---
+++ SpringBootGitDemoEclipse/src/main/java/com/example/learn/SpringBootGitDemoEclipseApplication.java
@@ -0,0 +1,14 @@
+package com.example.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitDemoEclipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitDemoEclipseApplication.class, args);
+		System.out.println("hello sir");
+	}
+
+}
